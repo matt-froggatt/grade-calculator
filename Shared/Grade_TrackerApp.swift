59323e9a96071d9929f8f02b1886020f2664bd6f
Matthew Froggatt <mattfr@live.ca>
2021-03-09 19:06:27 -0500

    Initial commit

---
+++ Shared/Grade_TrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Grade_TrackerApp.swift
+//  Shared
+//
+//  Created by Matthew Froggatt on 2021-03-09.
+//
+
+import SwiftUI
+
+@main
+struct Grade_TrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
