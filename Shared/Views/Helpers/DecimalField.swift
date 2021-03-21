9f6d0decbe276c2e871bf29ea018a328c7157ea3
Matthew Froggatt <mattfr@live.ca>
2021-03-21 17:39:59 -0400

    Filter input in decimal field

--- Shared/Views/Helpers/DecimalField.swift
+++ Shared/Views/Helpers/DecimalField.swift
@@ -12,31 +12,45 @@ struct DecimalField: View {
     var message: String
     @Binding var number: Decimal
     @State private var strNumber = ""
-
+    
     var body: some View {
         TextField(message, text: $strNumber)
             .keyboardType(.numberPad)
+            .onAppear {
+                self.strNumber = !number.isZero ? "\(NSDecimalNumber(decimal: number))" : ""
+            }
             .onReceive(Just(strNumber)) { newValue in
-                    self.strNumber = newValue
+                let filtered = newValue.filter { "0123456789.".contains($0) }
+                if filtered != newValue {
+                    self.strNumber = filtered
+                } else {
                     let tmp = Decimal(string: self.strNumber)
                     number = tmp != nil ? tmp! : number
                 }
+            }
     }
 }
 
 struct DecimalFieldPreviewWrapper: View {
-    @State var number: Decimal
+    @State var number1: Decimal
+    @State var number2: Decimal
     
     var body: some View {
         VStack {
-            DecimalField(message: "Test message", number: $number)
-            Text("\(NSDecimalNumber(decimal: number))")
+            VStack {
+                DecimalField(message: "No start number", number: $number1)
+                Text("\(NSDecimalNumber(decimal: number1))")
+            }
+            VStack {
+                DecimalField(message: "Start number", number: $number2)
+                Text("\(NSDecimalNumber(decimal: number2))")
+            }
         }
     }
 }
 
 struct DecimalField_Previews: PreviewProvider {
     static var previews: some View {
-        DecimalFieldPreviewWrapper(number: 0)
+        DecimalFieldPreviewWrapper(number1: 0, number2: 2)
     }
 }
